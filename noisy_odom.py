86aa08e980c596d28356a89f45305d4756da9871
loganhartford <logan.hartford@outlook.com>
2025-04-03 11:42:13 -0400

    more realistic noise model

--- noisy_odom.py
+++ noisy_odom.py
@@ -14,13 +14,22 @@ class NoisyOdometry(Node):
 
         self.odom_pub = self.create_publisher(Odometry, "/noisy_odom", 10)
 
-        self.twist_linear_noise_stddev = 0.1
-        self.twist_angular_noise_stddev = 0.05
+        self.theta = 0.15
+        self.mu = 0.0
+        self.dt = 0.1
+        self.sigma_linear = 0.1
+        self.sigma_angular = 0.05
 
-        self.get_logger().info("Noisy Odometry Node Started")
+        self.ou_noise_linear = 0.0
+        self.ou_noise_angular = 0.0
 
-        self.odom_logger = CSVLogger("odom.csv", ["x", "y", "v", "w"])
-        self.noisy_logger = CSVLogger("noisy_odom.csv", ["x", "y", "v", "w"])
+        self.get_logger().info("Noisy Odometry Node Started (using OU process noise)")
+
+        self.odom_logger = CSVLogger("csv/odom.csv", ["x", "y", "v", "w"])
+        self.noisy_logger = CSVLogger("csv/noisy_odom.csv", ["x", "y", "v", "w"])
+
+    def ornstein_uhlenbeck(self, x, theta, mu, sigma, dt):
+        return x + theta * (mu - x) * dt + sigma * np.sqrt(dt) * np.random.randn()
 
     def odom_callback(self, msg):
         raw_x = msg.pose.pose.position.x
@@ -28,14 +37,18 @@ class NoisyOdometry(Node):
         raw_v = msg.twist.twist.linear.x
         raw_w = msg.twist.twist.angular.z
 
+        self.ou_noise_linear = self.ornstein_uhlenbeck(
+            self.ou_noise_linear, self.theta, self.mu, self.sigma_linear, self.dt)
+        self.ou_noise_angular = self.ornstein_uhlenbeck(
+            self.ou_noise_angular, self.theta, self.mu, self.sigma_angular, self.dt)
+
         noisy_msg = Odometry()
         noisy_msg.header = msg.header
-
-
         noisy_msg.pose.pose = msg.pose.pose
+
         noisy_twist = deepcopy(msg.twist.twist)
-        noisy_twist.linear.x += np.random.normal(0, self.twist_linear_noise_stddev)
-        noisy_twist.angular.z += np.random.normal(0, self.twist_angular_noise_stddev)
+        noisy_twist.linear.x += self.ou_noise_linear
+        noisy_twist.angular.z += self.ou_noise_angular
         noisy_msg.twist.twist = noisy_twist
 
         self.odom_logger.log([raw_x, raw_y, raw_v, raw_w])
