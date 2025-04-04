aee74bfef123f0a0eae0d8190b2863dae0845d64
loganhartford <logan.hartford@outlook.com>
2025-04-04 11:53:24 -0400

    ukf sim data

--- utils/config.py
+++ utils/config.py
@@ -6,8 +6,8 @@ from .constants import ControllerType, LocalizationMode, PathType
 class Config(BaseModel):
     localization_mode: LocalizationMode = LocalizationMode.UKF
     controller_type: ControllerType = ControllerType.PID
-    path_type: PathType = PathType.CIRCLE
-    training_iteration: int = 0
+    path_type: PathType = PathType.ZIGZAG
+    training_iteration: int = 1
     klp: float = 0.2 
     klv: float = 0.5 
     kli: float = 0.2 
